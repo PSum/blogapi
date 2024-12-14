f49f0fa5e612afd3f11a9318b34e3867ad912ff6
PSum <sum.philipp@gmail.com>
2024-12-14 17:19:24 +0100

    update
    basic backend working

--- backend/prisma/migrations/20241214154719_init/migration.sql
+++ backend/prisma/migrations/20241214154719_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -10,7 +10,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Post" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "authorId" TEXT NOT NULL,
+    "authorName" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -26,8 +26,11 @@ CREATE TABLE "Comment" (
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
 -- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorName_fkey" FOREIGN KEY ("authorName") REFERENCES "User"("username") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/src/routes/apiRoutes.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+
+
+router.get('/', async (req, res) => {
+    try {
+        const data = await prisma.user.findMany({})
+        res.status(200).send(data);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('There has been an error' + err)
+  }
+})
+
+router.get('/user/:username/posts', async (req, res) => {
+  try {
+    const { username } = req.params;
+    // console.log(`\n ${username} \n `);
+
+    const userWithPosts = await prisma.user.findUnique({
+      where: { username: username },
+      include: { posts: true }, // Include the related posts
+    });
+
+    if (!userWithPosts) {
+      return res.status(404).send('User not found');
+    }
+
+    res.status(200).send(userWithPosts.posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('There has been an error');
+  }
+});
+
+
+router.post('/addUser', async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const data = await prisma.user.create({
+      data: {
+        username: username,
+      }
+    })
+  res.status(201).send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('There has been an error')
+  }
+})
+
+router.post('/addPost', async (req, res) => {
+  try {
+    const { postname , author, content } = req.body;
+
+    const data = await prisma.post.create({
+      data: {
+        name: postname,
+        authorName: author,
+        content: content,
+      }
+    })
+  res.status(201).send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('There has been an error')
+  }
+})
+
+module.exports = router;
